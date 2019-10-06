773cda6dc45e89141a601c53ab894b7b0881fd7b
Igor Martucelli <martucelli.igor@gmail.com>
2019-10-06 15:05:05 -0300

    Scope: ADDED SOCKE.IO TO USE REALTIME COMMUNICATION (WEBSOCKET)

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,12 +1,26 @@
 import "./styles.css";
 import React from "react";
-import api from "./../../services/api";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import websocket from "socket.io-client";
+import { useEffect, useState, useMemo } from "react";
+
+// Services import
+import api from "./../../services/api";
 
 export default function Dashboard()
 {
     const [spots, setSpots] = useState([]);
+    const [bookings, setBookings] = useState([]);
+
+    const user_id = localStorage.getItem("user");
+    const ws = useMemo(() => websocket("http://192.168.0.9:3333", { query: { user_id }}), [user_id]);
+
+    useEffect(() => {        
+        ws.on("booking_request", booking =>
+        {
+            setBookings([...bookings, booking]);
+        });
+    }, [bookings, ws]);
 
     useEffect(() => {
         async function loadSpots() {
@@ -14,16 +28,39 @@ export default function Dashboard()
             const { data } = await api.get("/dashboard", {
                 headers: { user_id }
             });
-            
-            console.log(data);            
+                      
             setSpots(data);           
         }
 
         loadSpots();
     }, []);
 
+    async function handleAccept(id)
+    {
+        await api.post(`/bookings/${id}/approvals`);
+        setBookings(bookings.filter(booking => booking._id !== id));
+    }
+
+    async function handleReject(id)
+    {
+        await api.post(`/bookings/${id}/rejections`);
+        setBookings(bookings.filter(booking => booking._id !== id));
+    }
+
     return (
         <>
+            <ul className="notifications">
+                { bookings.map(booking => (
+                    <li key={booking._id}>
+                        <p>
+                            <strong>{booking.user.email}</strong> is requesting a book on <strong>{booking.spot.company}</strong> to date: <strong>{booking.date}</strong>
+                        </p>
+                        <button onClick={() => handleAccept(booking._id)} className="accept">ACEITAR</button>
+                        <button onClick={() => handleReject(booking._id)} className="reject">REJEITAR</button>
+                    </li>
+                )) }
+            </ul>
+
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
